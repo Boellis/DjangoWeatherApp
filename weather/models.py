9430d4f742163e091a64cbe2f633cb414b2dfcc5
boellis <boellis@memphis.edu>
2019-01-06 14:47:36 -0600

    Addded functionality to retrieve weather from openweather api

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    #Objective: To be able to store city names in table
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'cities'
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+import requests
+from .models import City
 
 # Create your views here.
 def index(request):
-    return render(request,'weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=9c0c4ad1549fe55a212bd50fd12e3047'
+    city = "Memphis"
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': r['name'],
+            'temperature': r['main']['temp'],
+            'description':r["weather"][0]['description'] ,
+            'icon': r['weather'][0]['icon']
+
+        }
+        weather_data.append(city_weather)
+
+
+    context = {'city_weather': city_weather}
+    #print(city_weather)
+    return render(request,'weather/weather.html',context)
